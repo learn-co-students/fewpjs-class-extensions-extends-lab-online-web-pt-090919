eb6436252a75cea091cad653d547c234f4c3060a
fasnimo <fasnimo@gmail.com>
2020-05-05 12:05:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+class Polygon {
+    constructor(side) {
+        this.side = side
+    }
+    get countSides(){
+        return this.side.length
+    } 
+
+    get perimeter(){
+        return this.side.reduce(sideCount)
+        function sideCount(total, num) {
+            return total + num
+        }
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        return this.countSides === 3 && this.perimeter % 3 === 0
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return this.countSides === 4 && this.perimeter % 4 === 0     
+    }
+
+    get area() {
+        let length = this.perimeter / 4
+        let width = length
+        return length * width
+    }
+}
